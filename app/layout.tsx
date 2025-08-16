15ffb3e40f25ef9438b19db4269bf5fd6616eef5
Lily123xoxo <lily722022@gmail.com>
2025-08-16 17:39:21 +1000

    feat: layout: added cloudflare script for data tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,8 +11,22 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en" data-bs-theme="dark">
       <body>
         {children}
-        <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" strategy="afterInteractive" />
+        <Script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+          strategy="afterInteractive"
+        />
         <Script src="/js/index.js" strategy="afterInteractive" />
+
+        {/* Cloudflare Web Analytics */}
+        <Script
+          defer
+          src="https://static.cloudflareinsights.com/beacon.min.js"
+          strategy="afterInteractive"
+          data-cf-beacon={JSON.stringify({
+            token: process.env.NEXT_PUBLIC_CF_ANALYTICS_TOKEN,
+            spa: true,
+          })}
+        />
       </body>
     </html>
   );
