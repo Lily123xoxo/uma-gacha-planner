55f6fb447c913dfb807054bdcc7eeab5efa43254
Lily123xoxo <lily722022@gmail.com>
2025-08-16 18:27:05 +1000

    fix(layout): update Bootstrap JS integrity hash for security

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Bootstrap JS */}
         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-Gsz0p6U5m7cJqk3tCgy8o5o9k2bX8l3tqM0n5vYk1k3q2qfD0v2mX8b9tR0Uu3QF"
+          integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
           crossOrigin="anonymous"
           strategy="afterInteractive"
         />
