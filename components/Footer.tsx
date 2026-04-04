46dcfc8a6ba0a445d395cb77af8be2777c4f8abc
Lily123xoxo <lily722022@gmail.com>
2026-04-04 12:02:27 +1100

    Update footer to include github link.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,10 +7,23 @@ export default function Footer() {
                     Have a feature you would like to see?
                 </a>
             </span>
+
+            <span className="github-link" style={{ marginLeft: "1rem" }}>
+                <a
+                  href="https://github.com/Lily123xoxo/uma-gacha-planner"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View on GitHub ↗
+                </a>
+            </span>
         </div>
+
         <div className="footer-text">
-            <p className="last-updated">Website was last updated on: 5 December 2025. <br></br>
-            Umaplanner is not affiliated with the developers of Uma Musume. All trademarks and copyrights of Cygames, Inc are their own.</p>
+            <p className="last-updated">
+              Website was last updated on: 5 December 2025. <br />
+              Umaplanner is not affiliated with the developers of Uma Musume. All trademarks and copyrights of Cygames, Inc are their own.
+            </p>
         </div>
     </footer>
   );
